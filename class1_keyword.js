bd991da5c6647b710e7350b8e7b1dd199de2467e
saurabh kumar <saurabhkumar9493@gmail.com>
2024-04-18 11:06:43 +0530

    data

---
+++ class1_keyword.js
@@ -0,0 +1,95 @@
+
+
+// console.log(1+1)
+// console.log("aman")
+// console.log(8*5)
+
+ 
+//      let a = 10
+//      let b = "suhana"
+
+//     console.log(a)
+//     console.log(b)
+
+
+
+
+// let Naina = 10
+// console.log(Naina);
+
+// let a = 123456; 
+// console.log(a);
+// let b = 10; 
+// console.log(b)
+
+// let a = "aman";
+// let b = 'raman';
+// console.log(a);
+// console.log(b);
+
+
+// let a = 10;
+// var b = 20;
+// const c = 30;
+
+// console.log(a,b,c);
+
+// var a = 10;
+// var a = 20;
+//     a = 30;
+
+// let a  = 10;
+//     a  = 20;
+//     a = 30;
+
+// const a = 10;
+// var a = 10;
+// let a  = 20;
+
+
+// console.log(a);
+
+
+    //  var a = 10;
+        // let a = 20;
+        // const a = 30;
+
+     {
+        // console.log(a);
+     }
+
+
+     {
+        // var a = 10;
+        // let a = 20;
+        // const a = 30;
+        // console.log(a)
+     }
+    //  console.log(a)
+
+
+
+    //  var a = 10;
+    
+    // let a = 10;
+
+    //  {
+    //    let a = 20;
+    //     console.log(a)
+       
+    //  }
+ 
+    //   a = 30;
+     
+// const a = 10;
+//      a = 20;
+//      console.log(a)
+
+
+        
+    const a = 10;
+
+    {
+        // a = 20;
+    }
+    console.log(a)
---
+++ class2_dataType.js
@@ -0,0 +1,51 @@
+// Data types
+
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Undefined
+// 5.Null
+// 6.BigInt
+
+// 7.Object
+// 8.Array
+// 9.Symbols
+
+
+// 1.Number
+
+    //  let a = 20;
+    //  let b = 3;
+    //  let c = a+b;
+
+    //     console.log(a%b); 
+
+
+    //   let age = 10;
+        //   age  = age+10; 
+         // age+=10 //age = age+10
+        //    age = age-5;
+        //    age-=5;
+        // age = age*10;
+        // age*=10;
+        // age = age/2;
+        // age/=2;
+
+        //   console.log(age)
+
+        // let age  = 20;
+        //     age = age+1;
+        //     // age++;
+        //     console.log(age)
+
+
+        // let a = 10;
+        // let b = a++;  //post increment
+        
+        // console.log(b);
+        // console.log(a)
+
+        let a = 10;
+        let b = ++a;  //pre-increment a=a+1
+        console.log(a);
+        console.log(b);
