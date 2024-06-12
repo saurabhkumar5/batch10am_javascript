2b7d4f42ced317de84e1c6365d448e21a858c723
saurabh kumar <saurabhkumar9493@gmail.com>
2024-06-12 10:08:56 +0530

    completejs

---
+++ class12_trycatch.js
@@ -0,0 +1,16 @@
+
+
+console.log("hello");
+console.log("world");
+// try{
+//     console.log("kullu")
+// }catch{
+//     console.log("error")
+// }
+let a = 10
+try {
+    console.log(a)
+} catch {
+    console.log("error")
+}
+console.log("manali");
---
+++ class13_destructring.js
@@ -0,0 +1,30 @@
+
+// let a = "kullu";
+// let b = "manali";
+// let c = "shimla";
+// let d = "spiti";
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// let arr = ["kullu","manali","shimla","spiti"];
+
+    //   console.log(x[0])
+    //   console.log(x[1])
+    //   console.log(x[2])
+    //   console.log(x[3])
+    // let [a,b,c,d] = arr;
+    // console.log(a,b,c,d)
+
+                    
+             const obj  =   {
+                    naam:"govinda",
+                    hobby:"dancing",
+                    age:12
+                    
+                  }
+                
+                //   console.log(obj.naam,obj.age)
+
+                const {naam,hobby,age} = obj;
+                      console.log(naam)
